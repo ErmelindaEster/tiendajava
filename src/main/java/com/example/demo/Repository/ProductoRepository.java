6eea075b8d739789598f33a6d5b8d5bfcca475a6
unknown <tuemail@gmail.com>
2025-03-04 21:04:35 -0300

    Se realizo cambio de ubicacion de la carpeta Productoservice.java

--- src/main/java/com/example/demo/Repository/ProductoRepository.java
+++ src/main/java/com/example/demo/Repository/ProductoRepository.java
@@ -4,7 +4,7 @@ package com.example.demo.Repository;
 import com.example.demo.Models.Producto; //crud del modelo Producto
 import org.springframework.data.jpa.repository.JpaRepository; //libreria para hacer la conexion
 import java.util.List;
-//integer por el ID
+//integer por el IDS
 public interface ProductoRepository extends JpaRepository <Producto, Integer> {
    //producto activo
    List<com.example.demo.Models.Producto>findByEstadotrue();   
